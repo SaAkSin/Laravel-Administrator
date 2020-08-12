a1681fe6ccb574c58c5349b42729ea0aaf7e5539
SaAkSin <gslee@artgrammer.co.kr>
2020-08-12 21:47:59 +0900

    is_top_actions 속성 추가, true 인 경우 상단에 custom button 출력

--- src/views/templates/edit.php
+++ src/views/templates/edit.php
@@ -11,6 +11,22 @@
 		<!-- /ko -->
 	<!-- /ko -->
 
+    <?php
+    if($config->checkOption('is_top_actions')) {?>
+        <!-- ko if: $root[$root.primaryKey]() && actions().length -->
+        <div class="custom_buttons">
+            <!-- ko foreach: actions -->
+            <!-- ko if: has_permission && $root.actionPermissions[action_name] !== false -->
+            <input type="button" data-bind="click: function(){$root.customAction(true, action_name, messages, confirmation)}, value: title,
+																	attr: {disabled: $root.freezeForm() || $root.freezeActions()}" />
+            <!-- /ko -->
+            <!-- /ko -->
+        </div>
+        <!-- /ko -->
+        <?php
+    }
+    ?>
+
 	<!-- ko foreach: editFields -->
 		<!-- ko if: $data && ( $root[$root.primaryKey]() || editable ) && visible -->
 			<div data-bind="attr: {class: type}">
@@ -292,6 +308,22 @@
 			<!-- /ko -->
 		</div>
 	<!-- /ko -->
+
+    <?php
+    if(!$config->checkOption('is_top_actions')) {?>
+        <!-- ko if: $root[$root.primaryKey]() && actions().length -->
+        <div class="custom_buttons">
+            <!-- ko foreach: actions -->
+            <!-- ko if: has_permission && $root.actionPermissions[action_name] !== false -->
+            <input type="button" data-bind="click: function(){$root.customAction(true, action_name, messages, confirmation)}, value: title,
+																	attr: {disabled: $root.freezeForm() || $root.freezeActions()}" />
+            <!-- /ko -->
+            <!-- /ko -->
+        </div>
+        <!-- /ko -->
+        <?php
+    }
+    ?>
 
 	<div class="control_buttons">
 		<!-- ko if: $root[$root.primaryKey]() -->
