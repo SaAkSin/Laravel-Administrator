0d266667b6a4044509337a68f0c6b837ca13179c
SaAkSin(MacBook) <gslee@artgrammer.co.kr>
2026-06-10 17:41:48 +0900

    style: 이미지 업로드 버튼의 display flex 강제 오버라이드 및 줄바꿈 방지 추가

--- src/views/templates/edit.php
+++ src/views/templates/edit.php
@@ -215,11 +215,11 @@
 							<input type="file" :id="field.field_name + '_uploader'" 
 								   @change="uploadFile($event, field)" :disabled="freezeForm" style="display: none;" />
 							<button type="button" @click="$event.target.parentElement.querySelector('input[type=file]').click()"
-									:disabled="freezeForm" style="padding: 4px 10px; font-size: 11px; display: inline-flex; align-items: center; justify-content: center; gap: 4px; line-height: 1 !important;">
+									:disabled="freezeForm" style="padding: 4px 10px; font-size: 11px; display: inline-flex !important; align-items: center; justify-content: center; gap: 4px; line-height: 1 !important; white-space: nowrap !important;">
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" style="width: 12px; height: 12px; flex-shrink: 0; display: block;">
 									<path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
 								</svg>
-								<span style="display: inline-block; line-height: 1; vertical-align: middle;"><?php echo trans('administrator::administrator.uploadimage') ?></span>
+								<span style="display: inline-block; line-height: 1; vertical-align: middle; white-space: nowrap !important;"><?php echo trans('administrator::administrator.uploadimage') ?></span>
 							</button>
 							<div class="uploading" x-show="field.uploading"
 								 x-text="'<?php echo trans('administrator::administrator.imageuploading') ?>' + field.upload_percentage + '%'"></div>
